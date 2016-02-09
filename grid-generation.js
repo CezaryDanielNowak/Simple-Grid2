a1fadf23318d60f67cd0a1ba4ef80b742c779533
Cezary Daniel Nowak <cezary.nowak@gmail.com>
2016-02-09 17:02:02 +0100

    update bower & package

--- grid-generation.js
+++ grid-generation.js
@@ -1,24 +1,31 @@
 var MAX_COLUMNS = 12;
-var val;
-var out = {};
-var result = '';
 
-for (var i = 1; i <= MAX_COLUMNS; i++) {
-	for (var j = 1; j <= MAX_COLUMNS; j++) {
-		if (j > i) {
-			continue;
+function generateGrid(maxColumns) {
+	var val;
+	var out = {};
+	var result = '';
+	maxColumns || (maxColumns = MAX_COLUMNS);
+
+	for (var i = 1; i <= maxColumns; i++) {
+		for (var j = 1; j <= maxColumns; j++) {
+			if (j > i) {
+				continue;
+			}
+
+			val = ("" + (100*j/i)).substr(0,5);
+
+			out[val] || (out[val] = []);
+			out[val].push('col-' + j + '-' + i);
 		}
-
-		val = ("" + (100*j/i)).substr(0,5);
-
-		out[val] || (out[val] = []);
-		out[val].push('col-' + j + '-' + i);
 	}
+
+	for(var x in out) {
+		result += '.' + out[x].join(', .');
+		result += ' {\n\twidth: ' + x + '%;\n}\n';
+	}
+
+	return result;
 }
 
-for(var x in out) {
-	result += '.' + out[x].join(', .');
-	result += ' {\n\twidth: ' + x + '%;\n}\n';
-}
 
-console.log(result);
+module.exports = generateGrid
